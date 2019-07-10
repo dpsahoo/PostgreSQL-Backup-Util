58aba910c9dcdaa7d06a41be7eb7d4222e2d0478
Durga Sahoo <dpsahoo@gmail.com>
2019-07-10 18:43:07 +1000

    Implemented S3 interaction

--- src/pgbackup/storage.py
+++ src/pgbackup/storage.py
@@ -1,5 +1,13 @@
+def test_durga():
+	printf('test_durga')	
+
+#Define local storage function
 def local(infile, outfile):
 	outfile.write(infile.read())
 	outfile.close()
 	infile.close()
-	
+
+#Define AWS S3 storage function
+def s3(client, infile, bucket, name):
+	client.upload_fileobj(infile, bucket, name)
+
